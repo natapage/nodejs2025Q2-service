5f38532df6a4c90dd9f7449d265790e44a4986e2
Natalia Efimova <n.efimova@systtech.ru>
2025-05-31 14:47:33 +0500

    feat: implement OpenAPI/Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Home Library Service')
+    .setDescription('Home music library service')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  const docDir = path.join(__dirname, '..', 'doc');
+  if (!fs.existsSync(docDir)) {
+    fs.mkdirSync(docDir);
+  }
+  fs.writeFileSync(
+    path.join(docDir, 'api-generated.json'),
+    JSON.stringify(document, null, 2),
+  );
+
+  SwaggerModule.setup('doc', app, document);
+
+  await app.listen(port);
 }
 bootstrap();
