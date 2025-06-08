8b4b351f367e337050407d29fdbc0577b7cb8568
Natalia Efimova <n.efimova@systtech.ru>
2025-06-08 14:51:44 +0500

    fix: fix swagger setup

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,12 @@ import * as yaml from 'js-yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
   const port = process.env.PORT || 4000;
 
-  // Load the OpenAPI specification from the YAML file
   const docPath = join(__dirname, '..', 'doc', 'api.yaml');
   const docFile = await readFile(docPath, 'utf8');
   const document = yaml.load(docFile) as OpenAPIObject;
 
-  // Set up Swagger UI using the loaded document
   SwaggerModule.setup('doc', app, document);
 
   await app.listen(port);
